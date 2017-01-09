67fef8f87f2912ab7ed37a3fb2c40f7f9babe13a
KanchiShimono <shimono-kanchi-yc@ynu.jp>
2017-01-09 15:28:47 +0900

    Add path file for zsh ‘paths.zsh’

---
+++ _zsh/paths.zsh
@@ -0,0 +1,9 @@
+typeset -U path PATH fpath
+
+export PATH="$PATH:$HOME/dev/src/github.com/KanchiShimono/dotfiles/bin"
+export PATH="$PATH:$HOME/.vim/syntax_checker"
+export GOPATH="$HOME/dev"
+export PATH="$PATH:$GOPATH/bin"
+
+# source /opt/intel/compilers_and_libraries_2017/mac/mkl/bin/mklvars.sh intel64 ilp64
+# source /opt/intel/compilers_and_libraries_2017/mac/bin/compilervars.sh intel64
