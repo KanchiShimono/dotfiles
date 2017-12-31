05603777560c623af096cfce08a5a9ab49ebda53
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2018-01-01 01:03:43 +0900

    Add path for building gonvim

--- _zsh/paths.zsh
+++ _zsh/paths.zsh
@@ -6,6 +6,7 @@ export GOPATH="$HOME/dev"
 export GOOS="darwin"
 export GOARCH="amd64"
 export PATH="$PATH:$GOPATH/bin"
+export QT_HOMEBREW=true
 
 # source /opt/intel/compilers_and_libraries_2017/mac/mkl/bin/mklvars.sh intel64 ilp64
 # source /opt/intel/compilers_and_libraries_2017/mac/bin/compilervars.sh intel64
