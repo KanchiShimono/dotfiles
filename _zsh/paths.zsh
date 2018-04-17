30c5c844781690e177223f26ff66bd56436a513f
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2018-04-18 01:47:57 +0900

    Add a path for built haskell binary

--- _zsh/paths.zsh
+++ _zsh/paths.zsh
@@ -2,6 +2,7 @@ typeset -U path PATH fpath
 
 export PATH="$PATH:$HOME/dev/src/github.com/KanchiShimono/dotfiles/bin"
 export PATH="$PATH:$HOME/.vim/syntax_checker"
+export PATH="$PATH:$HOME/.local/bin"
 export GOPATH="$HOME/dev"
 export GOOS="darwin"
 export GOARCH="amd64"
