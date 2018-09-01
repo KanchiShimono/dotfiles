c2af553e37e67223e2fd5ff35f8af9095ba0ffe8
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2018-09-01 18:07:15 +0900

    Add python3 modules for scraping in install_modules.sh

--- install_modules.sh
+++ install_modules.sh
@@ -17,7 +17,7 @@ npm install --global create-react-app
 
 # packages for python
 # ------------------------------------------------------------------------------
-pip3 install numpy pandas matplotlib ipython tqdm Sphinx jupyter flake8
+pip3 install numpy pandas matplotlib ipython tqdm Sphinx jupyter flake8 beautifulsoup4 requests lxml
 pip2 install numpy pandas matplotlib ipython tqdm Sphinx jupyter
 # python2 -m pip install ipykernel
 # python2 -m ipykernel install --user
