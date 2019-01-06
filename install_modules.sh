5806b79414c174e755c15e949e6d3e378dc0a023
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2019-01-07 04:07:20 +0900

    Update python module in install_modules.sh
    Add mypy that static type checker for Python.

--- install_modules.sh
+++ install_modules.sh
@@ -17,7 +17,7 @@ npm install --global create-react-app
 
 # packages for python
 # ------------------------------------------------------------------------------
-pip3 install numpy pandas matplotlib ipython tqdm Sphinx jupyter flake8 beautifulsoup4 requests lxml selenium yapf
+pip3 install numpy pandas matplotlib ipython tqdm Sphinx jupyter flake8 beautifulsoup4 requests lxml selenium yapf mypy
 pip2 install numpy pandas matplotlib ipython tqdm Sphinx jupyter selenium yapf
 # python2 -m pip install ipykernel
 # python2 -m ipykernel install --user
