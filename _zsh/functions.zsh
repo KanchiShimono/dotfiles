8b23691f63865bbe44891b5601e090b6c9546459
Kanchi Shimono <dev.kanchi.shimono@gmail.com>
2020-09-01 17:51:56 +0900

    Add zsh function to copy contents of file to clipboad

--- _zsh/functions.zsh
+++ _zsh/functions.zsh
@@ -63,3 +63,8 @@ function cdd () {
 fh() {
   print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
 }
+
+# ccp - copy file to clipboad
+ccp() {
+	bat $* | pbcopy
+}
